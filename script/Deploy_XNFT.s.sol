79a3f1b85130444618257749d5181d6061e2efd7
Suyash <iitjeesuyash2023@gmail.com>
2024-10-18 07:58:28 +0530

    Initial Commit

--- script/Deploy_XNFT.s.sol
+++ script/Deploy_XNFT.s.sol
@@ -8,7 +8,7 @@ contract DeployXNFT is Script {
     function run() public {
         uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
         vm.startBroadcast(deployerPrivateKey);
-        
+
         address ccipRouterAddressEthereumSepolia = 0x0BF3dE8c5D3e8A2B34D2BEeB17ABfCeBaf363A59;
         address linkTokenAddressEthereumSepolia = 0x779877A7B0D9E8603169DdbD7836e478b4624789;
         uint64 chainSelectorEthereumSepolia = 16015286601757825753;
@@ -19,10 +19,7 @@ contract DeployXNFT is Script {
             chainSelectorEthereumSepolia
         );
 
-        console.log(
-            "XNFT deployed to ",
-            address(xNft)
-        );
+        console.log("XNFT deployed to ", address(xNft));
 
         vm.stopBroadcast();
     }
